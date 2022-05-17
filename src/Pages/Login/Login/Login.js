76fb8bc996fc8e36162710ebb05499d32ac429c2
haniful-islam <haniful360@gmail.com>
2022-05-17 21:31:52 +0600

    login page added

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import './Login.css';
 
 const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate();
+    // const location = useLocation();
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        // console.log(email, password);
+      
+    }
+
+    const navigateRegister = () => {
+        navigate('/register')
+    }
     return (
-        <div>
-            <h2>Please Login</h2>
+        <div className="width mx-auto border border-info rounded-3 p-5">
+            <h1 className="text-info text-center">Please Login</h1>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control ref={emailRef} type="email"   placeholder="Enter email" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control ref={passwordRef} type="password"  placeholder="Password" required />
+                </Form.Group>
+
+                <Button className="w-100 bg-info text-white" type="submit">
+                    Login
+                </Button>
+            </Form>
+            {/* {errorElement} */}
+            <p className="mt-2">New to a Bike Labs? <Link to='/register' className="text-info pe-auto text-decoration-none " onClick={navigateRegister}>Please Register</Link></p>
+            {/* <p className="mt-2">Forget Password? <button className="text-info pe-auto text-decoration-none btn btn-link" onClick={resetPassword}>Reset Password</button></p> */}
+           
         </div>
     );
 };
