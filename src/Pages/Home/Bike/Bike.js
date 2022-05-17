952de4fddf50b9c6e4244d8c1dedcc4b747ba756
haniful-islam <haniful360@gmail.com>
2022-05-17 18:29:55 +0600

    banner added

--- src/Pages/Home/Bike/Bike.js
+++ src/Pages/Home/Bike/Bike.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import './Bike.css';
 
 const Bike = ({bike}) => {
-    const {id, name, price, description, quality, supplier, img} = bike;
+    const {id, name, price, description, quality, Supplier, img} = bike;
     return (
-        <div>
-            <img width="370" height="350" src={img} alt=""/>
-            <h2 className="text-info">{name}</h2>
+        <div className="single-bike">
+            <img width="400" height="350" src={img} alt=""/>
+            <h2 className="text-info my-2">{name}</h2>
             <p>Price:${price}</p>
             <p>{description}</p>
-            <h4>{quality}</h4>
-            <h3>{supplier}</h3>
+            <h5>Quality:<small className="text-info my-2">{quality}</small></h5>
+            <h5>Supplier:<small className="text-info my-2">{Supplier}</small></h5>
+            <button className="btn btn-info text-white my-2">Update</button>
         </div>
     );
 };
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 import Bikes from '../Bikes/Bikes';
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <Bikes></Bikes>
         </div>
     );
