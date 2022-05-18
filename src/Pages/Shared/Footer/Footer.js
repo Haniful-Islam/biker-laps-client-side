c703c415dadbd7c8952a2c59599f0d94a13ef596
haniful-islam <haniful360@gmail.com>
2022-05-18 20:29:48 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
     return (
         <div>
-            <p>copy right @2022</p>
+            <div className="text-center bg-dark  mt-3 py-3">
+                <h3 className="text-info">Biker Labs</h3>
+                <p className="text-white">Copyright &copy;{year} Biker Labs</p>
+                <p className="text-white">All rights reserved</p>
+            </div>
         </div>
     );
 };
