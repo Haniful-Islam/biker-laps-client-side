204a2c85259e48cc22c465ef404475d951d8293c
haniful-islam <haniful360@gmail.com>
2022-05-19 22:34:48 +0600

    my items added

--- src/Pages/MyItems/MyItems.js
+++ src/Pages/MyItems/MyItems.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MyItems = () => {
     return (
-        <div>
-            <h2>My Items Added</h2>
+        <div className="my-5 text-center w-100 ">
+            <button className="btn btn-info my-5 mx-2 py-2 px-5 text-white fs-5"><Link className="pe-auto text-decoration-none text-white" to ="/all-items">All Items</Link></button>
+            <button className="btn btn-info my-5 mx-2 py-2 px-5 text-white fs-5"><Link className="pe-auto text-decoration-none text-white" to ="/addbike">Add Items</Link></button>
+            <button className="btn btn-info my-5 py-2 px-5 text-white fs-5"><Link className="pe-auto text-decoration-none text-white" to ="/manage">Manage Items</Link></button>
+            
         </div>
     );
 };
